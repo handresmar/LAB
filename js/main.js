c0c9848b81baa5676cf84c30d7f042cd52c9b30c
Hugo Andrés Martínez Díaz <hamd@hamd>
2019-04-02 17:29:41 -0500

    popup terminado

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-(function(){})();
+(function(){
+	var btnAddSp = document.getElementById('add'), 
+		overlay = document.getElementById('overlay'),
+		btnClPu = document.getElementById('puBtnCer');
+
+	btnAddSp.addEventListener('click', function(){
+		overlay.classList.add('active');
+	});
+	btnClPu.addEventListener('click', function(){
+		overlay.classList.remove('active');
+	});
+
+
+})();	
+/*
+(function(){
+})();
+*/
