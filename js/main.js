e9dd1b3424dd3c99bc190975a0f8e6e32c834834
Hugo Andrés Martínez Díaz <hamd@hamd>
2019-04-02 19:44:55 -0500

    iniciando importacion de muestras

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 (function(){
-	var btnAddSp = document.getElementById('add'), 
+	var btnAddSp = document.getElementById('add'),
+		btnPuAdd = document.getElementById('puAdd'), 
 		overlay = document.getElementById('overlay'),
 		btnClPu = document.getElementById('puBtnCer');
+	
+	var tabla = document.getElementsById('sampList'),
+		sp = document.createElement("tr"),
+		spCod=document.createElement("td"),
+		spTip=document.createElement("td"),
+		spDes=document.createElement("td"),
+		spCant=document.createElement("td");
 
 	btnAddSp.addEventListener('click', function(){
 		overlay.classList.add('active');
@@ -9,6 +17,14 @@
 	btnClPu.addEventListener('click', function(){
 		overlay.classList.remove('active');
 	});
+
+	btnClPu.addEventListener('click', addtabla());
+
+
+	function addtabla(){
+		
+
+	}
 
 
 })();	
