912866e374a0653f324307b892e88b30924ed2ed
Hugo Andrés Martínez Díaz <hamd@hamd>
2019-04-03 17:08:31 -0500

    Funcionando hasta agregar elementos a la tabla

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 (function(){
 	var btnAddSp = document.getElementById('add'),
 		btnPuAdd = document.getElementById('puAdd'), 
-		overlay = document.getElementById('overlay'),
-		btnClPu = document.getElementById('puBtnCer');
+		btnClPu = document.getElementById('puBtnCer'),
+		btnpdf = document.getElementById('pdfBtn'),
+
+		overlay = document.getElementById('overlay');
+		
 	
-	var tabla = document.getElementsById('sampList'),
-		sp = document.createElement("tr"),
-		spCod=document.createElement("td"),
-		spTip=document.createElement("td"),
-		spDes=document.createElement("td"),
-		spCant=document.createElement("td");
+	var tabla = document.getElementById('sampList');
+
+	var inputCod=document.getElementById('puCod'),
+		inputTipo=document.getElementById('puTipo'),
+		inputCant=document.getElementById('puCant'),
+		inputDesc=document.getElementById('des_pop');
 
 	btnAddSp.addEventListener('click', function(){
 		overlay.classList.add('active');
@@ -18,14 +21,49 @@
 		overlay.classList.remove('active');
 	});
 
-	btnClPu.addEventListener('click', addtabla());
+	btnPuAdd.addEventListener('click', comprobar);
 
+	function comprobar(){
+			if (inputCod.value==="" || inputTipo.value==="" || inputCant.value==="" || inputDesc.value===""){
+				alert('Comprelete los espacios');
+			} else{
+				addsmp();
+			}
+	};
 
-	function addtabla(){
+	function addsmp(){
+		var cod = document.createTextNode(inputCod.value),
+			tipo=document.createTextNode(inputTipo.value),
+			cant=document.createTextNode(inputCant.value),
+			desc=document.createTextNode(inputDesc.value);
+
+		var sp = document.createElement("tr"),
+			spCod=document.createElement("td"),
+			spTip=document.createElement("td"),
+			spDes=document.createElement("td"),
+			spCant=document.createElement("td");
+
+		spCod.appendChild(cod);
+		spTip.appendChild(tipo);
+		spDes.appendChild(desc);
+		spCant.appendChild(cant);
 		
 
-	}
+		sp.appendChild(spCod);
+		sp.appendChild(spTip);
+		sp.appendChild(spDes);
+		sp.appendChild(spCant);
 
+		sp.classList.add('smp');
+		sp.addEventListener('click', delSmpl);
+
+		tabla.appendChild(sp);
+		overlay.classList.remove('active');
+	};
+
+	function delSmpl(){
+		this.parentNode.removeChild(this);
+	};
 
 })();	
 /*
